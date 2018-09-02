e0ec7457dd50a7e80d5dfed69f88d3f7c69b7a98
Andre <andre@it-dengler.de>
2018-09-02 07:43:47 +0200

    added ToDo list

--- index.php
+++ index.php
@@ -10,13 +10,22 @@
 		<h1>Web Noticiation Demo</h1>
 		<h2>Tested with Chrome, Edge, Opera, Firefox (all Win10) and Chrome for Android</h2>
 		<p>Send subscription and notification (optional get browser permission & subscribe to browser dependent service) via php backend to local browser.</p>
-		<p>Service Workers are NOT supported with Safari / IOS Safari at the moment!</p> 
+		<p>Service Workers are NOT supported with Safari / IOS Safari at the moment!</p>
 
 		<h2>De-/Activate Notifications</h2>
+		<p>Toggle notifications by clicking the bell icon. A test notification is send on each activation click (with a configured delay of 5 seconds on server side).
+		Open the developer tools (network, console, application) for debugging un-/subscribe.</p>
 		<div><input type="image" id="notifyToggle" src="images/notifications_off.png"></input></div>
 
         <h2>"Add to homescreen" support</h2>
         <p>Demo supports full screen mode if added to homescreen as app shortcut in Chrome for Android</p>
+
+		<h2>ToDo</h2>
+		<ul>
+			<li>Add working indicator during de-/activate</li>
+			<li>Rewrite and clean up code to build a standalone notification library</li>
+			<li>Source code formatting and comments</li>
+		</ul>
 
 		<script src="scripts/notifications.js"></script>
 		<script>
